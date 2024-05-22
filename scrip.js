6810efe4a80e797b92f60eb00803800682e84eeb
andres <andresagudelo785@gmail.com>
2024-05-21 21:00:49 -0500

    agregar imput

--- scrip.js
+++ scrip.js
@@ -9,14 +9,36 @@ const buttonObtener = document.getElementById("Button-obtener")
 //agregar evento al boton
 
 
-buttonObtener.addEventListener("click", () => {
+buttonObtener.addEventListener("click", async () => {
     console.log("obtener")
+    const pokemonData = await traerPokemon()
+
+    const pokemonArticle = document.getElementById("pokemon")
+    //crea un elemento html
+    const h3 = document.createElement("h3")
+    // le asigna un texto
+    h3.innerText = pokemonData.name
+    //agrega un elemento como hijo
+    pokemonArticle.appendChild(h3)
+
+    // crear elemento img
+    
+    const img = document.createElement("img")
+    img.src = pokemonData.sprites.front_default
+    pokemonArticle.appendChild(img)
+
+
+
 })
 
 const traerPokemon = async () => {
-
-    const respuestaPokemon = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu") 
+    const leerImput = document.getElementById("NombrePokemon")
+    let nombrePokemon = leerImput.value
+    const respuestaPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`)
     const datosPokemon = await respuestaPokemon.json()
     console.log(datosPokemon)
+    return datosPokemon
 }
-traerPokemon()
+
+
+
